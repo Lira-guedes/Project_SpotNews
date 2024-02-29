6ca6d6cd7cd22f549df689be8854416cab95c016
Lira Guedes <liracrg@gmail.com>
2024-02-29 15:45:20 -0300

    feat: Cria a página de Detalhes

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
-from news.views import home
+from news.views import home, news_details
 from django.urls import path
 
 urlpatterns = [
-  path("", home, name="home-page")
+  path("", home, name="home-page"),
+  path('news/<int:id>', news_details, name='news-details-page'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from news.models import News
 
 
 def home(request):
     news = News.objects.all()
     return render(request, "home.html", context={"news": news})
+
+
+def news_details(request, id):
+    return render(request, 'news_details.html',
+                  context={'news': get_object_or_404(News, pk=id)})
